e3db45dc089e309d652ef6229c765e274dabf320
choiminjun <mj010504@naver.com>
2025-05-30 00:31:11 +0900

    chore: 임시 주석처리 - FCM 등록 토큰 Log

--- feature/main/src/main/java/com/example/main/MainActivity.kt
+++ feature/main/src/main/java/com/example/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -44,8 +43,6 @@ import com.example.navigation.MissionGraph
 import com.example.navigation.NavigationEvent
 import com.example.navigation.NavigationHelper
 import com.example.navigation.shouldHideBottomBar
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -60,15 +57,15 @@ class MainActivity : ComponentActivity() {
 
         installSplashScreen()
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                return@OnCompleteListener
-            }
-
-            val token = task.result
-            Log.d("traceMessaging", token.toString())
-
-        })
+//        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+//            if (!task.isSuccessful) {
+//                return@OnCompleteListener
+//            }
+//
+//            val token = task.result
+//            Log.d("traceMessaging", token.toString())
+//
+//        })
 
         if (intent.extras != null) { // 백그라운드 알림으로 앱에 진입
             handleNotificationIntent(intent, viewModel.navigationHelper)
