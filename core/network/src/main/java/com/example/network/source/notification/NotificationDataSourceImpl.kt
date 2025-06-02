ad366faec36122109de164063ca1a13f613909a0
choiminjun <mj010504@naver.com>
2025-06-02 21:02:59 +0900

    feat: FCM 토큰 저장 API 연동

--- core/network/src/main/java/com/example/network/source/notification/NotificationDataSourceImpl.kt
+++ core/network/src/main/java/com/example/network/source/notification/NotificationDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.example.network.source.notification
 
 import com.example.network.api.TraceApi
+import com.example.network.model.notification.PostDeviceTokenRequest
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.coroutines.Dispatchers
@@ -11,19 +12,18 @@ class NotificationDataSourceImpl @Inject constructor(
     private val traceApi: TraceApi,
     private val firebaseMessaging: FirebaseMessaging,
 ) : NotificationDataSource {
-    override suspend fun updateDeviceToken(token: String): Result<Unit> {
-       return Result.success(Unit)
-    }
+    override suspend fun updateDeviceToken(token: String): Result<Unit> =
+        traceApi.postDeviceToken(postDeviceTokenRequest = PostDeviceTokenRequest(token))
 
     override suspend fun postDeviceToken(): Result<Unit> {
         val token = getDeviceToken()
-        return Result.success(Unit)
+        return traceApi.postDeviceToken(postDeviceTokenRequest = PostDeviceTokenRequest(token))
     }
 
     override suspend fun getDeviceToken(): String = withContext(Dispatchers.IO) {
         try {
             Tasks.await(firebaseMessaging.token)
-        } catch (e : Exception) {
+        } catch (e: Exception) {
             throw Exception("Failed to get FCM token", e)
         }
     }
