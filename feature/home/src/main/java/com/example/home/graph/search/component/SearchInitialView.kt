4f1c24201b7e53627fcc1a77371338c88407c231
choiminjun <mj010504@naver.com>
2025-06-02 21:06:21 +0900

    feat: 최근 검색어를 눌러서 검색할 때 키보드 내리기

--- feature/home/src/main/java/com/example/home/graph/search/component/SearchInitialView.kt
+++ feature/home/src/main/java/com/example/home/graph/search/component/SearchInitialView.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import com.example.common.util.clickable
 import com.example.designsystem.theme.Background
@@ -112,11 +113,14 @@ private fun RecentKeyword(
     onSearch: (String) -> Unit,
     removeKeyword: (String) -> Unit,
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Card(
         modifier = Modifier
             .background(Background)
             .clickable(isRipple = true) {
                 onSearch(value)
+                keyboardController?.hide()
             },
         shape = RoundedCornerShape(8.dp),
         colors = CardColors(
